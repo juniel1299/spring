d3b2f894209ada86bc457ea7f73cc0c3cc110839
juniel1299 <youngs5440@gmail.com>
2024-05-14 14:45:36 +0900

    Initial commit

---
+++ BeginTest/src/main/java/com/test/begin/di2/Service.java
@@ -0,0 +1,26 @@
+package com.test.begin.di2;
+
+public class Service {
+	
+	private Hong hong;
+	
+	public Service(Hong hong) {
+		this.hong = hong;
+	}
+	
+	public void work() {
+				
+		hong.coding();
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
